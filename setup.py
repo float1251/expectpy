b0f1fafa686b5b09db7783f3e9e4aa36f7db79e3
float1251 <taka.05022002@gmail.com>
2014-08-06 22:21:08 +0900

    versionを修正

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="expectpy",
-    version="0.0.2",
+    version="0.0.3",
     author="takahiro iwatani",
     author_email="taka.05022002@gmail.com",
     url="https://github.com/float1251/expectpy",
